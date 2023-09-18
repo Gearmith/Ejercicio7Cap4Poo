fe690be14e1b871a356f14032e5e0011ca6d67b1
johnferneymontoyaramirez <jfmontoya@ins.com.co>
2023-09-18 18:01:26 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+Este es el ejercicio 7 correspondiente al capitulo 4
---
+++ src/main/java/john/ejercicio7cap4poo/Ejercicio7Cap4Poo.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package john.ejercicio7cap4poo;
+
+/**
+ *
+ * @author johnferneymontoyaramirez
+ */
+public class Ejercicio7Cap4Poo {
+
+    public static void main(String[] args) {
+	    Formulario form = new Formulario();
+	    form.setVisible(true);
+    }
+}
